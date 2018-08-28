3dc3be4e273ad0385dd0518ef2951879b166dbeb
Thiago de Arruda <tpadilha84@gmail.com>
2018-08-28 15:47:57 -0300

    Refactor build scripts for packaging/distribution

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import glob
 import os
 import os.path
 import shutil
+import sys
 
 from setuptools import setup
 from setuptools.extension import Extension
@@ -11,7 +12,8 @@ from setuptools.command.sdist import sdist
 from distutils.command.clean import clean
 
 
-VERSION = '0.0.1'
+VERSION = '1.0.0'
+MPACK_VERSION = '1.0.5'
 REPO = 'https://github.com/libmpack/libmpack-python'
 
 
@@ -26,63 +28,108 @@ class Clean(clean):
         clean.run(self)
 
 
+extension_src = 'mpack/_mpack.c'
+extensions = [Extension("mpack._mpack", [extension_src])]
+
+
+def _download_mpack():
+    import tarfile
+    import shutil
+    if sys.version_info >= (3, 0):
+        import urllib.request as urllib
+    else:
+        import urllib
+    url = 'https://github.com/libmpack/libmpack/archive/{}.tar.gz'.format(
+            MPACK_VERSION)
+    print('downloading libmpack...')
+    file_tmp = urllib.urlretrieve(url, filename=None)[0]
+    print('extracting libmpack...')
+    tar = tarfile.open(file_tmp)
+    tar.extractall('mpack')
+    directory = glob.glob('mpack/libmpack*')[0]
+    shutil.move(directory, 'mpack/mpack-src')
+
+def _autopxd():
+    from autopxd import translate
+    # due to some current limitations in autopxd, we must change
+    # directories to ensure the pxd is generated with the correct includes
+    cwd = os.getcwd()
+    os.chdir('mpack')
+    mpack_src = 'mpack-src/src/mpack.c'
+    with open(mpack_src) as f:
+        hdr = f.read()
+    with open('_cmpack.pxd', 'w') as f:
+        f.write(translate(hdr, mpack_src))
+    os.chdir(cwd)
+
+def _cythonize():
+    from Cython.Build import cythonize
+    kwargs = {
+        'gdb_debug': True,
+        'language_level': 3
+    }
+    if os.getenv('NDEBUG', False):
+        kwargs['gdb_debug'] = False
+    cythonize([Extension('mpack._mpack', ['mpack/_mpack.pyx'])], **kwargs)
+
+def _should_download_mpack():
+    return not os.path.exists('mpack/mpack-src/src/mpack.c')
+
+def _should_autopxd():
+    return not os.path.exists('mpack/_cmpack.pxd')
+
+def _should_cythonize():
+    try:
+        import Cython.Build
+    except ImportError:
+        return False
+    return (not os.path.exists(extension_src)
+            or os.environ.get('CYTHONIZE_MPACK', None) is not None)
+
 def with_hooks(cmdclass):
-    def _autopxd():
-        from autopxd import translate
-        # due to some current limitations in autopxd, we must change
-        # directories to ensure the pxd is generated with the correct includes
-        cwd = os.getcwd()
-        os.chdir('mpack')
-        mpack_src = 'mpack-src/src/mpack.c'
-        with open(mpack_src) as f:
-            hdr = f.read()
-        with open('_cmpack.pxd', 'w') as f:
-            f.write(translate(hdr, mpack_src))
-        os.chdir(cwd)
-
-    def _cythonize():
-        try:
-            from Cython.Build import cythonize
-        except ImportError:
-            return
-        kwargs = {
-            'gdb_debug': True,
-            'language_level': 3
-        }
-        if os.getenv('NDEBUG', False):
-            kwargs['gdb_debug'] = False
-        cythonize([Extension('mpack._mpack', ['mpack/_mpack.pyx'])], **kwargs)
-
     class Sub(cmdclass):
-        def build_extensions(self):
-            _autopxd()
-            _cythonize()
-            cmdclass.build_extensions(self)
-
         def run(self):
+            if _should_cythonize():
+                _cythonize()
             cmdclass.run(self)
+
     return Sub
 
 
-extensions = [Extension("mpack._mpack", ['mpack/_mpack.c'])]
+def datafiles():
+    if _should_download_mpack():
+        _download_mpack()
+    if _should_autopxd():
+        _autopxd()
+    if _should_cythonize():
+        _cythonize()
+    dataexts  = (".c", ".h", ".pxd", ".pyx")
+    datafiles = []
+    getext = lambda filename: os.path.splitext(filename)[1]
+    for datadir in ['mpack']:
+        datafiles.extend([(
+            root, [os.path.join(root, f)
+            for f in files if getext(f) in dataexts])
+            for root, dirs, files in os.walk(datadir)])
+    return datafiles
 
 
-setup(
-    name="mpack",
-    version=VERSION,
-    description="Python binding to libmpack",
-    packages=['mpack'],
-    ext_modules=extensions,
-    install_requires=['future'],
-    url=REPO,
-    download_url='{0}/archive/{1}.tar.gz'.format(REPO, VERSION),
-    license='MIT',
-    cmdclass={
-        'build_ext': with_hooks(build_ext),
-        'install': with_hooks(install),
-        'sdist': with_hooks(sdist),
-        'clean': Clean,
-    },
-    author="Thiago de Arruda",
-    author_email="tpadilha84@gmail.com"
-)
+if __name__ == '__main__':
+    setup(
+        name="mpack",
+        version=VERSION,
+        description="Python binding to libmpack",
+        packages=['mpack'],
+        ext_modules=extensions,
+        data_files=datafiles(),
+        install_requires=['future'],
+        url=REPO,
+        download_url='{0}/archive/{1}.tar.gz'.format(REPO, VERSION),
+        license='MIT',
+        cmdclass={
+            'build_ext': with_hooks(build_ext),
+            'clean': Clean,
+        },
+        author="Thiago de Arruda",
+        author_email="tpadilha84@gmail.com"
+    )
